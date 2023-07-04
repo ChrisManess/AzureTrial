e71d229a9375eac26c989325d0216b65aa5c5847
ChrisManess <maness@hey.com>
2023-07-03 22:46:23 -0400

    Adding initial finles

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
