59469d06b4d22064be94e43d5d34b0b1a5a596b6
ChrisManess <maness@hey.com>
2023-07-03 23:29:54 -0400

    ...

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('index.html', application_name=os.environ['EVN_SETTING'])
